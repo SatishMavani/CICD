3aea2118a182c70f5b7d94338487fd45de8e54ea
SatishMavani <satish.mavani@motifworks.com>
2019-12-13 15:00:00 +0530

    initial commit

---
+++ CICDSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CICDSample
+//
+//  Created by Satish Mavani on 13/12/19.
+//  Copyright © 2019 LTI. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
