79fe3fb19ccd3bb92e29a66c4403c2f0758db265
SatishMavani <satish.mavani@motifworks.com>
2019-12-19 13:41:22 +0530

    Added RED GREEN Blue configration settings

--- CICDSample/ViewController.swift
+++ CICDSample/ViewController.swift
@@ -13,6 +13,16 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        #if Red
+            self.view.backgroundColor = UIColor.red
+        #elseif Blue
+            self.view.backgroundColor = UIColor.blue
+        #elseif Green
+            self.view.backgroundColor = UIColor.green
+        #else
+            self.view.backgroundColor = UIColor.gray
+        #endif
     }
 
 
