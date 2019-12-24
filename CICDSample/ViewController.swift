f63ea256d396674374d79981bc7d729174a5b44d
SatishMavani <satish.mavani@motifworks.com>
2019-12-24 11:44:50 +0530

    Updated testcase

--- CICDSample/ViewController.swift
+++ CICDSample/ViewController.swift
@@ -14,8 +14,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        self.btnClickMe.isHidden = false
-        
+      
         #if Red
             self.view.backgroundColor = UIColor.red
         #elseif Blue
@@ -24,25 +23,16 @@ class ViewController: UIViewController {
             self.view.backgroundColor = UIColor.green
         #else
             self.view.backgroundColor = UIColor.gray
-             self.btnClickMe.isHidden = true
         #endif
         
-//        var nsDictionary: NSDictionary?
-//        if let path = Bundle.main.path(forResource: "Info", ofType: "plist") {
-//           nsDictionary = NSDictionary(contentsOfFile: path)
-//
-//            if let value = nsDictionary?["ShowClickButton"] as? Bool {
-//                if value {
-//                    self.btnClickMe.isHidden = false
-//                }
-//                else{
-//                    if (self.btnClickMe != nil) {
-//                        self.btnClickMe.isHidden = true
-//                    }
-//                }
-//            }
-//
-//        }
+        
+        if (self.btnClickMe != nil) {
+            #if Green
+                self.btnClickMe.isHidden = false
+            #else
+                self.btnClickMe.isHidden = true
+            #endif
+        }
         
     }
 }
